c94672f50eac32472214b53cc7bc0de1dba0a08e
Bilal <bilalallawala@gmail.com>
2021-09-18 22:55:27 -0400

    init

---
+++ packages/prismic-slice/.slicemachine/assets/slices/NewSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/NewSlice';
+
+export default {
+  title: 'slices/NewSlice'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"new_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Cultivate synergistic schemas","spans":[]}],"myImage":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1596195689404-24d8a8d1c6ea?w=900&h=500&fit=crop"},"description":[{"type":"paragraph","text":"Anim irure ut id in.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
